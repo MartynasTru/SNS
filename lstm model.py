7345a069c74848029520da3884a96dc1d9e2d0b0
Vansladd <92092512+Vansladd@users.noreply.github.com>
2023-04-07 10:13:30 +0100

    Update lstm model.py

--- lstm model.py
+++ lstm model.py
@@ -1,37 +1,46 @@
 import pandas as pd
 import numpy as np
+from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Bidirectional, Dropout
 from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
 
 # Load the dataset
-data = pd.read_csv("weather_data.csv", parse_dates=["datetime"], index_col="datetime")
-
-# Preprocessing
-scaler = MinMaxScaler()
-data_scaled = scaler.fit_transform(data)
-
+data = pd.read_csv("sample_data.csv", parse_dates=["datetime"], index_col="datetime")
+data = data.values
 # Create sequences
 def create_sequences(data, seq_length):
     x, y = [], []
-    for i in range(len(data) - seq_length):
-        x.append(data[i:i + seq_length])
+    for i in range(len(data) - seq_length-1):
+        sequence = data[i:i + seq_length]
+        x.append(sequence)
         y.append(data[i + seq_length])
     return np.array(x), np.array(y)
+seq_length = 7
 
-seq_length = 24
-x, y = create_sequences(data_scaled, seq_length)
 
 # Train-test split
-train_size = int(0.8 * len(x))
-x_train, x_test = x[:train_size], x[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
+# Split the data into training and test sets
+train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
 
+seq_length = 7
+x_train, y_train = create_sequences(train_data, seq_length)
+x_test, y_test = create_sequences(test_data, seq_length)
+print('Training data shape:', train_data.shape)
+print('Test data shape:', test_data.shape)
+
+# Reshape input data to have shape `(num_samples, seq_length, feature_dimension)`
+num_train_samples = x_train.shape[0]
+num_test_samples = x_test.shape[0]
+feature_dimension = x_train.shape[2]
+x_train = x_train.reshape((num_train_samples, seq_length, feature_dimension))
+x_test = x_test.reshape((num_test_samples, seq_length, feature_dimension))
 # Define the parameter grid for the grid search
 param_grid = {
-    'seq_length': [24, 48],
+    'seq_length': [7, 7],
     'num_units': [50, 100],
     'dropout': [0.2, 0.5],
     'learning_rate': [0.001, 0.01, 0.1],
@@ -47,12 +56,12 @@ def create_model(seq_length, num_units, dropout, learning_rate):
     model.add(Bidirectional(LSTM(units=num_units)))
     model.add(Dropout(dropout))
     model.add(Dense(units=data.shape[1]))
-    optimizer = Adam(lr=learning_rate)
+    optimizer = Adam(learning_rate=learning_rate)
     model.compile(optimizer=optimizer, loss='mean_squared_error')
     return model
 
 # Define the time series cross-validation
-tscv = TimeSeriesSplit(n_splits=5)
+tscv = TimeSeriesSplit(n_splits=3)
 
 # Initialize the grid search
 grid_search = GridSearchCV(estimator=KerasRegressor(build_fn=create_model), param_grid=param_grid, cv=tscv, verbose=1)
